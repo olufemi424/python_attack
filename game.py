36a6019d083116dd696ab0df30af875cffe493f8
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-04-04 11:41:53 -0500

    functions

--- game.py
+++ game.py
@@ -1,16 +1,17 @@
 from random import randint
 
-# variable
-# player_name = 'Olufemi'
-# player_attack = 10
-# player_heal = 16
-# health = 100
-
-# List
-# player = ['Olufemi', 10, 16, 100]
-
 # init game start
 game_running = True
+
+
+def calculate_monster_attack():
+    # create a random monster attack
+    return randint(monster['attack_min'], monster['attack_max'])
+
+
+def game_ends(winner_name):
+    print(winner_name + ' won ')
+
 
 # while game is running
 while game_running == True:
@@ -68,11 +69,12 @@ while game_running == True:
 
         # check player choice
         if player_choice == '1':
-            monster_attack = randint(
-                monster['attack_min'], monster['attack_max'])
+            monster_attack = calculate_monster_attack()  # monster attack function
 
+            # eack attack is fired both ways, the player get hit, and monster get hit
+            # and check if the moster is still with health or deal, the player get hit each time an attacl is fired as long as the monster is still alive
             monster['health'] = monster['health'] - player['attack']
-            # check monster health to determine if game player has won
+            # check monster health to determine if player has won
             if monster['health'] <= 0:
                 player_won = True
             else:
@@ -81,20 +83,24 @@ while game_running == True:
                 if player['health'] <= 0:
                     monster_won = True
 
+      # heal choice
         elif player_choice == '2':
-            monster_attack = randint(
-                monster['attack_min'], monster['attack_max'])
-            player['health'] = player['health'] + player['heal']
+            monster_attack = calculate_monster_attack()  # monster attack function
 
+         # increase player health with player heal value
+            player['health'] = player['health'] + player['heal']
+         # there is also a hit from the monster and check if player still got health
             player['health'] = player['health'] - monster_attack
             if player['health'] <= 0:
                 monster_won = True
-
+         # choice 3
+         # quit the game
         elif player_choice == '3':
             new_round = False
             game_running = False
             print('Game Exit')
 
+      #  choice not an option
         else:
             print('Input is not an option')
 
@@ -105,11 +111,11 @@ while game_running == True:
                   str(monster['health']) + ' health left.')
 
         elif player_won:
-            print(player['name'] + ' won ')
+            game_ends(player['name'])
             new_round = False
 
         if monster_won:
-            print('The Monster won')
+            game_ends(monster['name'])
             new_round = False
 
             # print(monster['health'])
